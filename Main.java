670117259f18a74524dc121a704fc2ead44d6362
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-14 17:12:10 -0400

    Update Main.java
    Added more choices and a Computer player.

--- Main.java
+++ Main.java
@@ -1,13 +1,34 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main
 {
 	public static void main(String[] args) {
+	    boolean gameOver = false;
+	    String computerType;
+	    String computer = "CPU";
+	    Random rand = new Random();
+	    int randomBeastForCPU = rand.nextInt(3)+1;
+	    
+        if(randomBeastForCPU == 1){
+            computerType = "fire";
+        }
         
-        Scanner sc = new Scanner(System.in);
-        System.out.println("What type of Beast do you want?\n-Fire- -Water- -Earth-");
-        String beastChoice = sc.nextLine();
-        
+        do{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("What type of Beast do you want?\n-Fire- -Water- -Earth-");
+            String beastChoice = sc.nextLine();
+            
+            if(beastChoice.equals("fire")){
+                System.out.println("Youve chosen the fire type beast!");
+                System.out.println("What is the name of your beast?");
+                String playerBeastName = sc.nextLine();
+            }else{
+                System.out.println("Invalid choice..try again.");
+            }
+            
+
+        }while(gameOver != true);
         
         
 	}
