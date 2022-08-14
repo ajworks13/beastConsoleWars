9a47fa38ec330c82d8c1946af5cd409fe2d20d08
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-14 17:54:22 -0400

    Update Main.java
    adjusted how a type is created.

--- Main.java
+++ Main.java
@@ -4,24 +4,29 @@ import java.util.Random;
 public class Main
 {
 	public static void main(String[] args) {
+	    Fire playerFireBeast = new Fire();
+	    //Water playerWaterBeast = new Water();
+	    //Earth playerEarthBeast = new Earth();
+	    Fire computerFireType = new Fire();
+	    
 	    boolean gameOver = false;
 	    String playerBeastName = null;
 	    //String computerType;
 	    String computer = "CPU";
 	    Random rand = new Random();
 	    int randomBeastForCPU = rand.nextInt(1)+1; // change to 3
-
+        /*
         if(randomBeastForCPU == 1){
-            Fire computerType = new Fire();
+            Fire computerFireType = new Fire();
         }
-        
+        */
         do{
             Scanner sc = new Scanner(System.in);
             System.out.println("What type of Beast do you want?\n-Fire- -Water- -Earth-");
             String beastChoice = sc.nextLine();
             
             if(beastChoice.equals("fire")){
-                Fire playerBeast = new Fire();
+               // Fire playerBeast = new Fire();
                 System.out.println("Youve chosen the fire type beast!");
                 System.out.println("What is the name of your beast?");
                 playerBeastName = sc.nextLine();
@@ -35,9 +40,10 @@ public class Main
             
             do{
                 System.out.println("Welcome and enjoy the game " + playerBeastName);
-                System.out.println("Player 1! " + playerBeastName + " you go first.");
-                System.out.println("You can type... -1 to regular hit   -2 to hard hit  -3 to special move hit  -4 to heal yourself");
-                String p1Choice = sc.nextInt();
+                System.out.println("Player 1! " + playerBeastName + " it is your turn.");
+                System.out.println("\n" + "Player HP: " + playerFireBeast.getHp() + " ---- CPU HP: " + computerFireType.getHp());
+                System.out.println("Type a number... (1) to regular hit   (2) to hard hit  (3) to special move hit  (4) to heal yourself");
+                int p1Choice = sc.nextInt();
                 
                 if(p1Choice == 1){
                     
