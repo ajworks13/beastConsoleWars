aa797e2798d05ab7150aed6ce7ec49246ecbf15b
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-14 17:35:33 -0400

    Update Main.java
    adding more decision making.

--- Main.java
+++ Main.java
@@ -5,13 +5,14 @@ public class Main
 {
 	public static void main(String[] args) {
 	    boolean gameOver = false;
-	    String computerType;
+	    String playerBeastName = null;
+	    //String computerType;
 	    String computer = "CPU";
 	    Random rand = new Random();
-	    int randomBeastForCPU = rand.nextInt(3)+1;
-	    
+	    int randomBeastForCPU = rand.nextInt(1)+1; // change to 3
+
         if(randomBeastForCPU == 1){
-            computerType = "fire";
+            Fire computerType = new Fire();
         }
         
         do{
@@ -20,12 +21,28 @@ public class Main
             String beastChoice = sc.nextLine();
             
             if(beastChoice.equals("fire")){
+                Fire playerBeast = new Fire();
                 System.out.println("Youve chosen the fire type beast!");
                 System.out.println("What is the name of your beast?");
-                String playerBeastName = sc.nextLine();
+                playerBeastName = sc.nextLine();
+                //System.out.println(playerBeast.getHp());
+
+                
             }else{
                 System.out.println("Invalid choice..try again.");
             }
+            
+            
+            do{
+                System.out.println("Welcome and enjoy the game " + playerBeastName);
+                System.out.println("Player 1! " + playerBeastName + " you go first.");
+                System.out.println("You can type... -1 to regular hit   -2 to hard hit  -3 to special move hit  -4 to heal yourself");
+                String p1Choice = sc.nextInt();
+                
+                if(p1Choice == 1){
+                    
+                }
+            }while(gameOver != true);
             
 
         }while(gameOver != true);
