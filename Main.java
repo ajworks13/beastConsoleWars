cc6fc09cc9d09af386e90bbbfdfbc9ebb7d2105d
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-14 16:47:21 -0400

    Create Main.java
    Just started.

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What type of Beast do you want?\n-Fire- -Water- -Earth-");
+        String beastChoice = sc.nextLine();
+        
+        
+        
+	}
+}
