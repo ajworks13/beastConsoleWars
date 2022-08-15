1afbe5baf61064c4749193faf3475df695b9e997
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-15 17:12:23 -0400

    Update Main.java
    Added more functionality with the AI, move decisions and winner.

--- Main.java
+++ Main.java
@@ -6,8 +6,26 @@ import java.util.Random;
  
  */
 
+
+
+
 public class Main
 {
+    // Global Vars
+    static boolean gameOver = false;
+    static boolean winner = false;
+    static String playerBeastName = null;
+    
+    public static void winner(int theHealth, String playerOrCpu){
+        System.out.println("Currently at: " + theHealth);
+        if(theHealth <= 0 && playerOrCpu.equals(playerBeastName)){
+            System.out.println("WE HAVE A WINNER!");
+            winner = true;
+            gameOver = true;
+
+        }
+    }
+    
 	public static void main(String[] args) {
 	    Fire playerFireBeast = new Fire();
 	    //Water playerWaterBeast = new Water();
@@ -16,10 +34,7 @@ public class Main
 	    //Water computerWaterType = new Water();
 	    //Earth computerEarthType = new Earth();
 	    
-	    boolean gameOver = false;
-	    String playerBeastName = null;
-	    //String computerType;
-	    String computer = "CPU";
+	    String computerBeastName = "CPU";
 	    Random rand = new Random();
 	    int randomBeastForCPU = rand.nextInt(1)+1; // change to 3
         /*
@@ -44,17 +59,36 @@ public class Main
                 System.out.println("Invalid choice..try again.");
             }
             
-            
+            System.out.println("Welcome and enjoy the game " + playerBeastName);
+
             do{
-                System.out.println("Welcome and enjoy the game " + playerBeastName);
                 System.out.println("Player 1! " + playerBeastName + " it is your turn.");
                 System.out.println("\n" + "Player HP: " + playerFireBeast.getHp() + " ---- CPU HP: " + computerFireType.getHp());
                 System.out.println("Type a number... (1) to regular hit   (2) to hard hit  (3) to special move hit  (4) to heal yourself");
                 int p1Choice = sc.nextInt();
                 
+                // HUMAN"s turn ---------------
                 if(p1Choice == 1){
-                    
+                    computerFireType.hit();
+                    computerFireType.getHp();
                 }
+                
+                winner(computerFireType.getHp(), computerBeastName);
+                int randomPowerSelectForCPU = rand.nextInt(4)+1; // change to 4
+
+                // COMPUTER's turn -------------
+                if(randomPowerSelectForCPU == 1){
+                    playerFireBeast.hit();
+                }else if(randomPowerSelectForCPU == 2){
+                    playerFireBeast.hardHit();
+                }else if(randomPowerSelectForCPU == 3){
+                    playerFireBeast.specialTypeHit();
+                }else if(randomPowerSelectForCPU == 4){
+                    computerFireType.heal();
+                }
+                
+                winner(playerFireBeast.getHp(), playerBeastName);
+                
             }while(gameOver != true);
             
 
