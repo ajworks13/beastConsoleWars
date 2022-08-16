486daab00d20e6d926002e460e6bc59315d5a89c
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-15 23:48:13 -0400

    Update Fire.java
    Added a cooldown functionality. Still incomplete.

--- Fire.java
+++ Fire.java
@@ -1,5 +1,7 @@
 public class Fire extends Beast{
     private int hp = 1000;
+    private byte hitCooldown = 0;
+    
     
     // getter
     public int getHp(){
@@ -10,27 +12,59 @@ public class Fire extends Beast{
         this.hp = remainingHp;
     }
     
+    public byte getCooldownCountDown(){
+        return hitCooldown;
+    }
     
     
-    
+    // constructor
     public Fire(){
         this.hp = hp;
     }
     
+    
+    
     public void hit(){
         hp-=100;
+        checkForCooldown();
     }
     
     public void hardHit(){
         hp-=250;
+        checkForCooldown();
     }
     
     public void specialTypeHit(){
-        System.out.println("Fire type special move!!");
-        hp-=400;
+        //System.out.println("Fire type special move!!");
+        //hp-=400;
+        //hitCooldown = 3;
+        checkForCooldown();
+        
+        if(hitCooldown == 0){
+            hp-=400;
+            hitCooldown = 3;
+            System.out.println(" -- Move under cooldown -- : " + hitCooldown);
+        }else if(hitCooldown > 0){
+            System.out.println(" -- Still under cooldown --");
+            hitCooldown-=1;
+        }
+        
+        
     }
     
     public void heal(){
         hp+=300;
+        checkForCooldown();
     }
+    
+    private void checkForCooldown(){
+        if(hitCooldown == 0){
+            System.out.println("Special move is ready!");
+        }
+    }
+    
+    
+    
+    
+    
 }
